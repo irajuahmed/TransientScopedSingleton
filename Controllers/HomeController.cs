3e7c61c4ed29043eb67f41ebb14975abc0ad1061
Raju Ahmed <raju.ahmed@datasoft-bd.com>
2021-01-03 16:32:15 +0600

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using TransientScopedSingleton.Models;
+
+namespace TransientScopedSingleton.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly ITransientService _transientService1;
+        private readonly ITransientService _transientService2;
+        private readonly IScopedService _scopedService1;
+        private readonly IScopedService _scopedService2;
+        private readonly ISingletonService _singletonService1;
+        private readonly ISingletonService _singletonService2;
+        public HomeController(ILogger<HomeController> logger,
+            ITransientService transientService1,
+            ITransientService transientService2,
+            IScopedService scopedService1,
+            IScopedService scopedService2,
+            ISingletonService singletonService1,
+            ISingletonService singletonService2)
+        {
+            _logger = logger;
+            _transientService1 = transientService1;
+            _transientService2 = transientService2;
+            _scopedService1 = scopedService1;
+            _scopedService2 = scopedService2;
+            _singletonService1 = singletonService1;
+            _singletonService2 = singletonService2;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.transient1 = _transientService1.GetOperationID().ToString();
+            ViewBag.transient2 = _transientService2.GetOperationID().ToString();
+            ViewBag.scoped1 = _scopedService1.GetOperationID().ToString();
+            ViewBag.scoped2 = _scopedService2.GetOperationID().ToString();
+            ViewBag.singleton1 = _singletonService1.GetOperationID().ToString();
+            ViewBag.singleton2 = _singletonService2.GetOperationID().ToString();
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ OperationService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TransientScopedSingleton
+{
+    public class OperationService : ITransientService, IScopedService, ISingletonService
+    {
+        Guid id;
+        public OperationService()
+        {
+            id = Guid.NewGuid();
+        }
+        public Guid GetOperationID()
+        {
+            return id;
+        }
+    }
+}
